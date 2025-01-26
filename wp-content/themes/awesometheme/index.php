ef4eec901678fc64089a92a08836ef009c0e02a4
faisalahammed044274 <faisalahammed044274@gmail.com>
2025-01-26 19:20:56 +0600

    Added posts

--- wp-content/themes/awesometheme/index.php
+++ wp-content/themes/awesometheme/index.php
@@ -1,3 +1,19 @@
 <?php get_header(); ?>
-<h2>This is my index.</h2>
+<?php
+if (have_posts()):
+
+    while (have_posts()):
+        the_post(); ?>
+        <hr>
+        <h2><?php the_title(); ?></h2>
+        <small>Posted on : <?php the_time('F j, Y'); ?></small>&nbsp;
+        <?php the_category(); ?></small>
+        <br>
+        <p><?php the_content(); ?></p>
+        <hr>
+
+<?php
+    endwhile;
+endif;
+?>
 <?php get_footer(); ?>
